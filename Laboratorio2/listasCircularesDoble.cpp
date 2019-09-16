9762235cd200a8e6dd585a83886b1f6ef459f20e
narommm <00113417@uca.edu.sv>
2019-09-16 16:14:20 -0600

    Tarea3 add Quicksort

--- Laboratorio2/listasCircularesDoble.cpp
+++ Laboratorio2/listasCircularesDoble.cpp
@@ -6,3 +6,17 @@ struct Nodo{
     Nodo* sig;
     Nodo* ant;
 }*inicio, *fin;
+
+class listasCirculares{
+    public:
+        Nodo* crearNodo(int valor){
+            Nodo *n= new Nodo;
+            n->dato=valor;
+            n->sig= nullptr;
+            n->ant=nullptr;
+            return n;
+        }
+        void mostrarListaAdelante(){
+            
+        }
+}
