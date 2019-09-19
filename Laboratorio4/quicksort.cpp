9605abd4c7d882f160835eb6c7fe01d086c2a53c
narommm <00113417@uca.edu.sv>
2019-09-18 22:19:44 -0600

    Missing Number

--- Laboratorio4/quicksort.cpp
+++ Laboratorio4/quicksort.cpp
@@ -1,47 +1,72 @@
+
+#include <cstdlib>
 #include <bits/stdc++.h>
 
-using​​namespace​ std;// Una función para cambiar dos elementos
-void ​swap(​int​* a, ​int​* b){
-int ​t = *a;
-*a = *b;
-*b = t;
+using namespace std;
+
+void swap(int* a, int* b){
+    int t=*a;
+    *a = *b;
+    *b =t;
 }
-int​ partition (​int​ arr[], ​int​ low, ​int​ high){
-    int​ pivot = arr[high]; // pivote
-    int​ i = (low - ​1​); // posición del primer elemento
-    for ​(​int​ j = low; j <= high -​ 1​; j++){
-        // Si el el elemento actual es menor que el pivote
-        if​ (arr[j] < pivot){
-            i++; // incrementar el index del menor elemento
-            swap(&arr[i], &arr[j]);
+
+int partition(int arr[], int low, int high){
+    int pivot = arr[high];
+    int i =(low -1);
+    
+    for(int j=low;j<=high-1;j++){
+        if(arr[j] < pivot){
+            i++;
+            swap(&arr[i],&arr[j]);
         }
     }
-    swap(&arr[i + ​1​], &arr[high]);
-    return​ (i +​ 1​);
+    swap(&arr[i+1], &arr[high]);
+    return(i+1);
 }
-void​ quickSort(​int​ arr[], ​int​ low, ​int​ high){
-    if​ (low < high){
-        /*pi es la partición del índice, arr[p] está en el lugar correcto */
-        int​ pi = partition(arr, low, high);
-        // Ordenando separadamente los elementos antes y después de la
-        // partición
-        quickSort(arr, low, pi -​ 1​);
-        quickSort(arr, pi +​ 1​, high);
+
+void quickSort(int arr[], int low, int high){
+    if(low<high){
+        int pi = partition(arr,low,high);
+        quickSort(arr, low,pi-1);
+        quickSort(arr, pi+1,high);
     }
 }
-/* Función para imprimir arreglo */
-void​ printArray(​int​ arr[], ​int​ size){
-    int​ i;
-    for​ (i = 0; i < size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+
+void missingNumber(int arr[], int size){
+    int i;
+    int mayor=0;
+    for(i=0;i<size;i++){
+        if(arr[i] > mayor){
+            mayor=arr[i];
+        }
+    }
+    mayor++;
+    while(mayor%2!=0 || mayor<0){
+        mayor++;
+    }
+    cout<<mayor;
 }
-// Código principal para utilizarlo
-int​ main(){
-int​ arr[] = {​10​,​ 7​, ​8​, ​9​, ​1​, ​5​};
-int​ n = ​sizeof​(arr) / ​sizeof​(arr[0]);
-quickSort(arr,​ 0​, n - ​1​);
-cout << "Sorted array: \n";
-printArray(arr, n);
-return​ 0;
+
+void printArray(int arr[], int size){
+    int i;
+    for(i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    
+}
+
+int main(int argc, char** argv) {
+    
+    int arr[]={1,3,3,3,6,7};
+    int n= sizeof(arr)/sizeof(arr[0]);
+    cout<<"Su arreglo es: ";
+    printArray(arr,n);
+    quickSort(arr,0,n-1);
+    cout<<" Arreglo Ordenado: ";
+    printArray(arr,n);
+    cout<<"El missing number es: ";
+    missingNumber(arr, n);
+
+    return 0;
 }
