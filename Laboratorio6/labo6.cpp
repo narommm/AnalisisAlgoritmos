7645c2992a199ccb275810207dbe56d05b0d9d90
narommm <00113417@uca.edu.sv>
2019-10-25 18:12:29 -0600

    Labo6

--- Laboratorio6/labo6.cpp
+++ Laboratorio6/labo6.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int n=3;
 int f1[3];
 int f2[3];
-string r1;
-string r2;
+string respuesta1;
+string respuesta2;
 
 int fastestWay(int a[2][3],int t[2][2],int e[2], int x[2]){
     f1[0]=e[0]+a[0][0];
@@ -18,31 +18,31 @@ int fastestWay(int a[2][3],int t[2][2],int e[2], int x[2]){
             stringstream s;
             s<<j;
             string str =s.str();
-            r1=r1+",a1"+str;
+            respuesta1=respuesta1+",a1"+str;
 
         }else{
             f1[j]=f2[j-1]+t[1][j-1]+a[0][j];
             stringstream ss;
             ss<<j-1;
             string str =ss.str();
-            r1=r1+",a2"+str+",t2"+str;
+            respuesta1=respuesta1+",a2"+str+",t2"+str;
         }
         if((f2[j-1]+a[1][j]) <= (f1[j-1]+t[0][j-1]+a[1][j])){
             f2[j]=f2[j-1]+a[1][j];
             stringstream s;
             s<<j;
             string str =s.str();
-            r2=r2+",a2"+str;
+            respuesta2=respuesta2+",a2"+str;
         }else{
             f2[j]=f1[j-1]+t[0][j-1]+a[1][j];
             stringstream ss;
             ss<<j-1;
             string str =ss.str();
-            r2=r2+",a1"+str+",t1"+str;
+            respuesta2=respuesta2+",a1"+str+",t1"+str;
         }
     }
-    cout<<"Respuesta 1: "<<r1<<endl;
-    cout<<"Respuesta 2: "<<r2<<endl;
+    cout<<"Respuesta 1: "<<respuesta1<<endl;
+    cout<<"Respuesta 2: "<<respuesta2<<endl;
     cout<<"Costo: ";
     return min(f1[n-1]+x[0],f2[n-1]+x[1]);
 
